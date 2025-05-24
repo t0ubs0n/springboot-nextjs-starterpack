dcb805abcc3a6616a2b16491d45bd9d37a881551
t0ubs0n <dev.toubson@gmail.com>
2025-05-24 21:17:50 +0200

    ✨ feat: integrate SpringDoc OpenAPI for API documentation
    - Added `springdoc-openapi` dependencies to `pom.xml`.
    - Configured OpenAPI settings in `application.yaml` to enable Swagger UI and API docs.
    - Introduced `OpenApiConfig` class to customize API documentation with security, metadata, and configurations.

---
+++ modulith/src/main/java/com/toubson/modulith/config/OpenApiConfig.java
@@ -0,0 +1,40 @@
+package com.toubson.modulith.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Modulith API")
+                        .description("Spring Boot Modulith API Documentation")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("T0ubs0n")
+                                .url("https://github.com/t0ubs0n"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
